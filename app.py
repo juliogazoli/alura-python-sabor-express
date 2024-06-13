3f3d9cbd999e1e708f05a799a1757047c39f85a9
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-06-13 12:03:27 -0300

    laco de repeticao for

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Sushi']
 
 
 def exibir_nome_do_programa():
@@ -39,9 +39,19 @@ def cadastrar_novo_restaurante():
         'Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal')
+    input('\nDigite uma tecla para voltar ao menu principal')
     main()
 
+
+def listar_restaurantes():
+    os.system('clear')
+    print('Listando os restaurantes\n')
+    
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
 
 def escolher_opcao():
     try:
@@ -51,7 +61,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
