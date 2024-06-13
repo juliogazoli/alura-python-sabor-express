c5db5cfb705dc4721239e61e9b357474a8067e41
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-06-13 12:40:16 -0300

    refatorando o codigo

--- app.py
+++ app.py
@@ -22,36 +22,42 @@ def exibir_opcoes():
 
 
 def finalizar_app():
-    os.system('clear')
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizando o app')
+
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
 
 
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
 
 
 def cadastrar_novo_restaurante():
-    os.system('clear')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input(
         'Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 
 def listar_restaurantes():
-    os.system('clear')
-    print('Listando os restaurantes\n')
-    
+    exibir_subtitulo('Listando os restaurantes')
+
     for restaurante in restaurantes:
         print(f'.{restaurante}')
-    
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+
+    voltar_ao_menu_principal()
+
 
 def escolher_opcao():
     try:
