cc794d01579684eebdadbe1f9a89b5486e062b31
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-06-14 12:11:17 -0300

    ativando restaurantes

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def cadastrar_novo_restaurante():
     }
     restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    
+
     voltar_ao_menu_principal()
 
 
@@ -74,6 +74,27 @@ def listar_restaurantes():
     voltar_ao_menu_principal()
 
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+
+    nome_restaurante = input(
+        'Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante[
+                'ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
+
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -84,7 +105,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
